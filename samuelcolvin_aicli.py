a87742c3deb8c61b95c9da999dbe0120407b6842
Justin Flannery <juftin@juftin.com>
2023-09-15 04:12:37 -0600

    auto-suggestion history (#3)
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>

--- samuelcolvin_aicli.py
+++ samuelcolvin_aicli.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import openai
 from prompt_toolkit import PromptSession
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.history import FileHistory
 from rich.console import Console
 from rich.live import Live
@@ -40,7 +41,7 @@ The current date and time is {datetime.now()} {now_utc.astimezone().tzinfo.tznam
 
     while True:
         try:
-            text = session.prompt('aicli ➤ ')
+            text = session.prompt('aicli ➤ ', auto_suggest=AutoSuggestFromHistory())
         except (KeyboardInterrupt, EOFError):
             return 0
 
